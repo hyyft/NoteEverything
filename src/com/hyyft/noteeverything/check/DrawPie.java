456692075922ef4f236ca17020ec4972c8f7543c
hyyft <425573057@qq.com>
2014-05-15 18:31:34 +0800

    完成基本功能，下一步计划完善天气

---
+++ src/com/hyyft/noteeverything/check/DrawPie.java
@@ -0,0 +1,245 @@
+package com.hyyft.noteeverything.check;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.renderer.DefaultRenderer;
+import org.achartengine.renderer.SimpleSeriesRenderer;
+
+import com.hyyft.noteeverything.dao.DaoDbHelper;
+import com.hyyft.noteeverything.dao.DayPlanDao;
+import com.hyyft.noteeverything.dao.DoWhatDao;
+import com.hyyft.noteeverything.dao.PlanDbHelperContract;
+import com.hyyft.noteeverything.modal.DayPlan;
+import com.hyyft.noteeverything.modal.DoWhat;
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+
+
+/**
+ * 画圆饼图
+ * @author Administrator
+ *
+ */
+public class DrawPie {
+
+	private List<String> bigTagList;
+	public static final int DRAW_PLANTODO=1;
+    public static final int DRAW_ACTULLY=2;
+    public static final int DRAW_DO=3;
+    
+    private int count = 1;
+	private Context context;
+	private GraphicalView graphicalView;
+	private CategorySeries dataset; 
+	private DefaultRenderer renderer;
+	private int[] colors = {Color.BLUE , Color.GREEN , Color.RED , Color.YELLOW , Color.CYAN , Color.GRAY
+							,Color.DKGRAY , Color.LTGRAY , Color.MAGENTA , Color.TRANSPARENT , Color.BLACK , Color.parseColor("#0099CC")};
+	public DrawPie( Context context , List<String> bigTagList){
+		this.context = context;
+		this.bigTagList = bigTagList;
+	}
+	
+	
+	
+	public GraphicalView drawPlan(String date , String bigTag , int tag){
+		dataset = buildCategoryDataset(date, bigTag ,tag);
+		renderer = buildCategoryRenderer(date , bigTag , count);
+		graphicalView=ChartFactory.getPieChartView(context, dataset, renderer);
+		return graphicalView;
+	}
+	public GraphicalView drawDowhat(String date  , String bigTag){
+		dataset = buildCategoryDataset(date, bigTag , DRAW_DO);
+		renderer = buildCategoryRenderer(date , bigTag , count);
+		graphicalView=ChartFactory.getPieChartView(context, dataset, renderer);
+		return graphicalView;
+	}
+	
+	
+	private CategorySeries buildCategoryDataset(String date , String bigTag , int tag ) {
+		CategorySeries series = new CategorySeries(bigTag);
+		Map<String, Integer > map ;
+		
+		if(tag == DRAW_PLANTODO )map = getPlanSituation(bigTag ,date);
+		else if(tag == DRAW_ACTULLY) map = getRealSituation(bigTag , date);
+		else map = getDoWhat(bigTag , date);
+		
+		Iterator<Map.Entry<String,Integer>> iterator = map.entrySet().iterator();
+		if(  !iterator.hasNext() ) {
+			series.add("无数据",1);
+			return series;
+		}
+		while(iterator.hasNext()){
+			++count;
+			
+			 Map.Entry<String, Integer> entry = (Map.Entry<String, Integer>) iterator.next();
+			 series.add(entry.getKey() , entry.getValue().intValue());
+		}
+		--count;
+		return series;
+	}
+	
+	private DefaultRenderer buildCategoryRenderer(String date , String bigTag , int n) {
+        DefaultRenderer renderer = new DefaultRenderer();
+       
+        renderer.setLegendTextSize(20);//设置左下角表注的文字大小
+        renderer.setLabelsColor(Color.BLACK);
+      //renderer.setZoomButtonsVisible(true);//设置显示放大缩小按钮  
+        renderer.setZoomEnabled(false);//设置不允许放大缩小.  
+          renderer.setChartTitleTextSize(30);//设置图表标题的文字大小
+          renderer.setChartTitle(bigTag);//设置图表的标题  默认是居中顶部显示
+          renderer.setLabelsTextSize(20);//饼图上标记文字的字体大小
+          renderer.setPanEnabled(false);//设置是否可以平移
+          renderer.setDisplayValues(true);//是否显示值
+          renderer.setClickEnabled(true);//设置是否可以被点击
+        renderer.setMargins(new int[] { 20, 30, 15,0 });
+        //margins - an array containing the margin size values, in this order: top, left, bottom, right
+        for (int j=1 ; j<=n ; j++) {
+          SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+          r.setColor(colors[j]);
+          renderer.addSeriesRenderer(r);
+        }
+        return renderer;
+      }
+	
+	private Map<String, Integer> getPlanSituation(String bigTag , String date){
+		Map<String, Integer > map ;
+		if( bigTag == "全部" ){
+			map = getPlanALl(date);
+		}
+		else {
+			map = getPlanByBigTag(bigTag, date);
+		}
+		
+		return map;
+	}
+	
+	private Map<String, Integer> getRealSituation(String bigTag , String date){
+		Map<String, Integer > map ;
+		if( bigTag == "全部" ){
+			map = getRealALl(date);
+		}
+		else {
+			map = getRealByBigTag(bigTag, date);
+		}
+		
+		return map;
+	}
+	
+	private Map<String, Integer> getDoWhat(String bigTag , String date){
+		Map<String, Integer > map ;
+		if( bigTag == "全部" ){
+			map = getDoALl(date);
+		}
+		else {
+			map = getDoByBigTag(bigTag, date);
+		}
+		
+		return map;
+	}
+	
+	
+	
+	private Map<String, Integer> getPlanALl(String date){
+		//在该处获取所有计划的情况
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		DayPlanDao dao = new DayPlanDao(context);
+		Integer integer;
+		List<DayPlan> list = dao.getAll(PlanDbHelperContract.PlanTableInfo.PLAN_TABLE_NAME, date);
+		for(int i=0 ; i < list.size() ; i++){
+			if( (integer = map.get(list.get(i).getBigTag()) )== null )
+				map.put( list.get(i).getBigTag(), list.get(i).getPlanTime() );
+			else 
+				map.put(list.get(i).getBigTag(), integer.intValue()+list.get(i).getPlanTime());			
+		}
+		return map;
+	}
+	private Map<String, Integer> getPlanByBigTag(String bigTag , String date){
+		//在该处获取按照bigtag分类的计划
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		DayPlanDao dao = new DayPlanDao(context);
+		Integer integer;
+		List<DayPlan> list = dao.getAll(PlanDbHelperContract.PlanTableInfo.PLAN_TABLE_NAME, date);
+		for(int i=0 ; i < list.size() ; i++){
+			if( ! list.get(i).getBigTag().equals(bigTag))continue;
+			if( (integer = map.get(list.get(i).getLitleTag()) ) == null )
+				map.put( list.get(i).getLitleTag(), list.get(i).getPlanTime() );
+			else 
+				map.put(list.get(i).getLitleTag(), integer.intValue()+list.get(i).getPlanTime());			
+		}
+		return map;
+	}
+	
+	private Map<String, Integer> getRealALl(String date){
+		//在该处获取所有计划的情况
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		DayPlanDao dao = new DayPlanDao(context);
+		Integer integer;
+		List<DayPlan> list = dao.getAll(PlanDbHelperContract.PlanTableInfo.PLAN_TABLE_NAME, date);
+		for(int i=0 ; i < list.size() ; i++){
+			if( (integer = map.get(list.get(i).getBigTag()) )== null )
+				map.put( list.get(i).getBigTag(), list.get(i).getRealTime() );
+			else 
+				map.put(list.get(i).getBigTag(), integer.intValue()+list.get(i).getRealTime());			
+		}
+		return map;
+	}
+	private Map<String, Integer> getRealByBigTag(String bigTag , String date){
+
+		//在该处获取按照bigtag分类的计划
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		DayPlanDao dao = new DayPlanDao(context);
+		Integer integer;
+		List<DayPlan> list = dao.getAll(PlanDbHelperContract.PlanTableInfo.PLAN_TABLE_NAME, date);
+		for(int i=0 ; i < list.size() ; i++){
+			if( ! list.get(i).getBigTag().equals(bigTag))continue;
+			if( (integer = map.get(list.get(i).getLitleTag()) ) == null )
+				map.put( list.get(i).getLitleTag(), list.get(i).getRealTime() );
+			else 
+				map.put(list.get(i).getLitleTag(), integer.intValue()+list.get(i).getRealTime());			
+		}
+		return map;
+	}
+	
+	private Map<String, Integer> getDoALl(String date){
+		//在该处获取所有计划的情况
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		DoWhatDao dao = new DoWhatDao(context);
+		Integer integer;
+		List<DoWhat> list = dao.getAll(PlanDbHelperContract.DoWhatTableInfo.TABLE_NAME, date);
+		for(int i=0 ; i < list.size()-1 ; i++){
+			
+			if( (integer = map.get(list.get(i).getBigTag()) )== null ){
+					map.put( list.get(i).getBigTag(), (int)( list.get(i+1).getBeginTime() - list.get(i).getBeginTime())/60000 );
+			}
+			else 
+				map.put(list.get(i).getBigTag(), integer.intValue()+(int)( list.get(i+1).getBeginTime() - list.get(i).getBeginTime())/60000 );			
+		}
+		return map;
+	}
+	private Map<String, Integer> getDoByBigTag(String bigTag , String date){
+
+		//在该处获取按照bigtag分类的计划
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		DoWhatDao dao = new DoWhatDao(context);
+		Integer integer;
+		List<DoWhat> list = dao.getAll(PlanDbHelperContract.DoWhatTableInfo.TABLE_NAME, date);
+		for(int i=0 ; i < list.size()-1 ; i++){
+			if( ! list.get(i).getBigTag().equals(bigTag))continue;
+			if( (integer = map.get(list.get(i).getLitleTag()) ) == null )
+				map.put( list.get(i).getLitleTag(), (int)( list.get(i+1).getBeginTime() - list.get(i).getBeginTime())/60000 );
+			else 
+				map.put(list.get(i).getLitleTag(), integer.intValue()+(int)( list.get(i+1).getBeginTime() - list.get(i).getBeginTime())/60000);			
+		}
+		return map;
+	}
+}
--- src/com/hyyft/noteeverything/fragment/CheckFragment.java
+++ src/com/hyyft/noteeverything/fragment/CheckFragment.java
@@ -1,22 +1,277 @@
 package com.hyyft.noteeverything.fragment;
 
-import com.hyyft.noteeverything.R;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.hyyft.noteeverything.R;
+import com.hyyft.noteeverything.check.DrawPie;
+import com.hyyft.noteeverything.myconst.PrefConst;
+import com.hyyft.noteeverything.plan.CreateTimeDialog;
+import com.hyyft.noteeverything.plan.CreateTimeDialog.CreateTimeDialogCallBack;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class CheckFragment extends Fragment {
+public class CheckFragment extends Fragment implements OnItemSelectedListener , 
+														CreateTimeDialogCallBack{
 
+	
+	private static final int PLANTEXTVIEW = 1;
+	private static final int DOTEXTVIEW = 2;
+	private  int whichSelected = PLANTEXTVIEW;
+	
+	private static final int PLANDOTEXTVIEW = 1;
+	private static final int ACTUALLDOTEXTVIEW = 2;
+	private  int whichSelected2 = PLANDOTEXTVIEW;
+	
+	private int drawWhich = DrawPie.DRAW_PLANTODO;
+	
+	private TextView dateTextView , planTextView , doTextView;
+	private TextView planDoTextView , actullyDoTextView;
+	private Spinner bigTagSpinner;
+	private LinearLayout  drawLayout , btnLayout;
+	private List<String> bigTagList;
+	private View subvView;
+	private ArrayAdapter<String> adapter;
+	//private Layout
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
+		View view = inflater.inflate(R.layout.checkfragment_layout, container, false);
 		
-		return inflater.inflate(R.layout.fragment2_layout, container, false);
+		subvView = LayoutInflater.from(getActivity()).inflate(R.layout.check_plan_and_actually, null);
+		findView(view);
+		initView();
+		
+		return view;
 	}
+	
+	
+	
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		bigTagSpinner.setSelection(0);
+	}
+
+
+
+	private void findView(View view){
+		dateTextView = (TextView)view.findViewById(R.id.check_tv_date);
+		planTextView = (TextView)view.findViewById(R.id.check_tv_plan);
+		doTextView = (TextView)view.findViewById(R.id.check_tv_do);
+		bigTagSpinner = (Spinner)view.findViewById(R.id.check_spinner_bigtag);
+		drawLayout = (LinearLayout)view.findViewById(R.id.check_layout_draw);
+		btnLayout = (LinearLayout)view.findViewById(R.id.check_layout_btn);
+		planDoTextView = (TextView)subvView.findViewById(R.id.check_tv_plantodo);
+		actullyDoTextView = (TextView)subvView.findViewById(R.id.check_tv_actuallytodo);
+		
+	}
+	private void initView(){
+		Time time = new Time();
+		time.setToNow();
+		dateTextView.setText(""+time.year+"-"+(time.month+1)+"-"+time.monthDay);
+		planTextView.setBackgroundColor(Color.parseColor("#999999"));
+		doTextView.setBackgroundColor(Color.parseColor("#CCCCCC"));
+		
+		btnLayout.addView(subvView);
+		planDoTextView.setBackgroundColor(Color.parseColor("#999999"));
+		actullyDoTextView.setBackgroundColor(Color.parseColor("#CCCCCC"));
+	
+		drawLayout.removeAllViews();
+		drawLayout.addView(new DrawPie(getActivity(), bigTagList)
+				.drawPlan(dateTextView.getText().toString(), "全部" , DrawPie.DRAW_PLANTODO) );
+		
+		adapter = getBigTag();
+		bigTagSpinner.setAdapter(adapter);
+		bigTagSpinner.setSelection(0);
+		
+		bigTagSpinner.setOnItemSelectedListener(this);
+		dateTextView.setOnClickListener(textViewListener);
+		planTextView.setOnClickListener(textViewListener);
+		doTextView.setOnClickListener(textViewListener);
+		planDoTextView.setOnClickListener(textViewListener);
+		actullyDoTextView.setOnClickListener(textViewListener);
+		
+	}
+	
+	/**
+	 * 
+	 * @return 返回一个给Spinner使用的adapter
+	 */
+	private ArrayAdapter<String> getBigTag(){
+		bigTagList = new ArrayList<String>();
+		SharedPreferences sharedPreferences = 
+				getActivity().getSharedPreferences(PrefConst.NAME,Context.MODE_PRIVATE);
+		//int count = sharedPreferences.getInt(PrefConst.BIGTAG_COUNT, 0);
+		String temp;
+		int i = 1;
+		bigTagList.add("全部");
+		while( true ){
+			temp = sharedPreferences.getString(""+i, "none");
+			//Log.i("yuan" , temp);
+			if( temp.equals("none") )break;
+			else bigTagList.add(new String(temp));
+			i++;
+		}
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),  
+                android.R.layout.simple_spinner_item, bigTagList); 
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); 
+		return adapter;
+	}
+	
+	
+	/**
+	 * 用于监听所有textView click的Listener
+	 */
+	private OnClickListener textViewListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch (v.getId()) {
+			case R.id.check_tv_date:
+				new CreateTimeDialog(getActivity(), CheckFragment.this).getDate();
+				break;
+			case R.id.check_tv_plan:
+				if( whichSelected == DOTEXTVIEW ){
+					whichSelected = PLANTEXTVIEW;
+					planTextView.setBackgroundColor(Color.parseColor("#999999"));
+					doTextView.setBackgroundColor(Color.parseColor("#CCCCCC"));
+					btnLayout.addView(subvView );
+					
+					drawLayout.removeAllViews();
+					drawLayout.addView(new DrawPie(getActivity(), bigTagList)
+							.drawPlan(dateTextView.getText().toString(), "全部" ,DrawPie.DRAW_PLANTODO) );
+					drawWhich = DrawPie.DRAW_PLANTODO;
+					bigTagSpinner.setSelection(0);
+				}
+				
+				
+				break;
+			case R.id.check_tv_do:
+				if( whichSelected == PLANTEXTVIEW ){
+					whichSelected = DOTEXTVIEW;
+					planTextView.setBackgroundColor(Color.parseColor("#CCCCCC"));
+					doTextView.setBackgroundColor(Color.parseColor("#999999"));
+					btnLayout.removeView(subvView);
+					drawLayout.removeAllViews();
+					drawLayout.addView(new DrawPie(getActivity(), bigTagList)
+					.drawDowhat(dateTextView.getText().toString(), "全部" ) );
+					drawWhich = DrawPie.DRAW_DO;
+					bigTagSpinner.setSelection(0);
+				}
+				break;
+				
+			case R.id.check_tv_plantodo:
+				//Toast.makeText(getActivity(), "hahah",Toast.LENGTH_LONG).show();
+				if( whichSelected2 == ACTUALLDOTEXTVIEW ){
+					whichSelected2 = PLANDOTEXTVIEW;
+					
+					planDoTextView.setBackgroundColor(Color.parseColor("#999999"));
+					actullyDoTextView.setBackgroundColor(Color.parseColor("#CCCCCC"));
+					
+					drawLayout.removeAllViews();
+					drawLayout.addView(new DrawPie(getActivity(), bigTagList)
+							.drawPlan(dateTextView.getText().toString(), "全部" ,DrawPie.DRAW_PLANTODO) );
+					drawWhich = DrawPie.DRAW_PLANTODO;
+					bigTagSpinner.setSelection(0);
+				}
+				break;
+			case R.id.check_tv_actuallytodo:
+				//Toast.makeText(getActivity(), "hahah",Toast.LENGTH_LONG).show();
+				if( whichSelected2 == PLANDOTEXTVIEW ){
+					whichSelected2 = ACTUALLDOTEXTVIEW;
+					
+					planDoTextView.setBackgroundColor(Color.parseColor("#CCCCCC"));
+					actullyDoTextView.setBackgroundColor(Color.parseColor("#999999"));
+					
+					drawLayout.removeAllViews();
+					drawLayout.addView(new DrawPie(getActivity(), bigTagList)
+							.drawPlan(dateTextView.getText().toString(), "全部" ,DrawPie.DRAW_ACTULLY) );
+					drawWhich = DrawPie.DRAW_ACTULLY;
+					bigTagSpinner.setSelection(0);
+				}
+				break;
+
+			default:
+				break;
+			}
+		}
+	};
+	
+	
+	
+	
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position,
+			long id) {
+		// TODO Auto-generated method stub
+		drawLayout.removeAllViews();
+		if( drawWhich == DrawPie.DRAW_ACTULLY ||  drawWhich == DrawPie.DRAW_PLANTODO){
+		drawLayout.addView(new DrawPie(getActivity(), bigTagList)
+				.drawPlan(dateTextView.getText().toString(),
+						adapter.getItem(position) , drawWhich) 
+						);
+		}
+		else {
+			drawLayout.addView(new DrawPie(getActivity(), bigTagList)
+			.drawDowhat(dateTextView.getText().toString(),
+					adapter.getItem(position)  )
+					);
+		}
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	@Override
+	public void dateDialogCallBack(String date) {
+		// TODO Auto-generated method stub
+		dateTextView.setText(date);
+	}
+
+	@Override
+	public void timeDialogCallBack(String time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void timeDialogCallBack(int hour, int minute) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ptimeDialogCallBack(int hour, int minute) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 	
 }
